7540876b3b4b55b473332c24b0175a74b6f1c6e9
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-03-05 09:06:56 -0300

    feat: create combineReducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
 import { createStore } from 'redux'
+import cart from './modules/cart/reducer'
+import rootReducer from './modules/rootReducer'
 
-const store = createStore(() => [])
+const store = createStore(rootReducer)
 
 export default store
