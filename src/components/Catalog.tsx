11ae61382d2109c0c28e6774d6e692d8ea2b386b
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-03-05 09:32:36 -0300

    feat: add json-server

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,10 +1,28 @@
-import { useSelector } from "react-redux"
+import { useEffect, useState } from "react"
+import { api } from "../services/api"
+import { IProduct } from "../store/modules/cart/types"
 
 export function Catalog() {
-  const store = useSelector(state => state)
+  const [catalog, setCatalog] = useState<IProduct[]>([])
 
-  console.log(store)
+  useEffect(() => {
+    async function loadCatalog() {
+      await api.get('products').then(res => setCatalog(res.data))
+    }
+
+    loadCatalog()
+  }, [])
+
   return (
-    <h1>catalog</h1>
+    <div>
+      <h1>catalog</h1>
+      {catalog.map(item => (
+        <article key={item.id}>
+          <strong>{item.title}</strong> {" - "}
+          <strong>{item.price}</strong> {" - "}
+          <button>Comprar</button>
+        </article>
+      ))}
+    </div>
   )
 }
