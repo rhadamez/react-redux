672b3f386453551e4736fe89bc417382f8a9619c
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-03-05 10:34:58 -0300

    feat: validate product amount in reducer

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,9 @@
 import { useSelector } from "react-redux"
+import { IState } from "../store"
+import { ICartItem } from "../store/modules/cart/types"
 
 export function Cart() {
-  const state = useSelector(state => state)
-  console.log(state)
+  const state = useSelector<IState, ICartItem[]>(state => state.cart.items)
 
   return (
     <table>
@@ -15,6 +16,14 @@ export function Cart() {
         </tr>
       </thead>
       <tbody>
+        {state.map(item => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.amount}</td>
+            <td>{(item.product.price * item.amount).toFixed(2)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   )
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -10,8 +10,18 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
     case 'ADD_PRODUCT_TO_CART': {
       const { product } = action.payload
 
+      const isAdded = state.items.findIndex(p => p.product.id === product.id)
+      const newProductsArray = [...state.items]
 
-      return {...state, items: [...state.items, { product, amount: 1 } ]}
+      if(isAdded !== -1) {
+        const currentProducot = state.items[isAdded]
+        currentProducot.amount = currentProducot.amount + 1
+        newProductsArray[isAdded] = currentProducot
+      } else {
+        newProductsArray.push({ product, amount: 1 })
+      }
+
+      return {...state, items: newProductsArray}
     }
 
     default: {
